63b3c98f6dc20b42cf2eef66ebe2cc451e97ed79
Thierry <dev@coworkingcafe.fr>
2025-07-13 17:51:15 +0200

    SAVE

--- src/app/(admin)/(dashboard)/pdf/config.ts
+++ src/app/(admin)/(dashboard)/pdf/config.ts
@@ -1,4 +1,4 @@
 // Configuration pour désactiver le pré-rendu des pages PDF
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 export const revalidate = false;
-export const fetchCache = 'force-no-store';
+export const fetchCache = "force-no-store";
